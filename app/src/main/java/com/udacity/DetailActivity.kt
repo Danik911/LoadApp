6fa22901075fe5756f0c289e8207aaa3b77deb2c
Daniil <porvipop@mail.ru>
2022-02-08 08:54:18 +0300

    Setup notificationManager

--- app/src/main/java/com/udacity/DetailActivity.kt
+++ app/src/main/java/com/udacity/DetailActivity.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_detail.*
 
+
+const val DETAIL_ACTIVITY_KEY = "Status"
+const val DETAIL_ACTIVITY_FILENAME_KEY = "Filename"
+
 class DetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
         setSupportActionBar(toolbar)
+
     }
 
 }
---
+++ app/src/main/java/com/udacity/NotificationManager.kt
@@ -0,0 +1,41 @@
+package com.udacity
+
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import androidx.core.app.NotificationCompat
+
+private const val NOTIFICATION_ID = 0
+
+fun NotificationManager.sendNotification(
+    channelId: String,
+    message: String,
+    context: Context,
+    fileName: String,
+    status: String
+) {
+    val detailActivityIntent = Intent(context, DetailActivity::class.java)
+    detailActivityIntent.putExtra(DETAIL_ACTIVITY_KEY, status)
+    detailActivityIntent.putExtra(DETAIL_ACTIVITY_FILENAME_KEY, status)
+
+    val buttonPendingIntent = PendingIntent.getActivity(
+        context,
+        NOTIFICATION_ID,
+        detailActivityIntent,
+        PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+    )
+    val builder = NotificationCompat.Builder(context, channelId)
+        .setSmallIcon(R.drawable.ic_assistant_black_24dp)
+        .setContentTitle(context.getString(R.string.notification_title))
+        .setContentText(message)
+        .setAutoCancel(true)
+        .addAction(
+            R.drawable.ic_assistant_black_24dp,
+            context.getString(R.string.notification_button),
+            buttonPendingIntent
+        )
+        .setPriority(NotificationCompat.PRIORITY_HIGH)
+
+    notify(NOTIFICATION_ID, builder.build())
+}
